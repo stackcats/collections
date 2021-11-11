4569e8b26b9bb703fa5f428d6d426857eed41454
stackcats <stackcats@foxmail.com>
2021-11-11 23:49:34 +0800

    feat: add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Collections.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/stackcats/collections",
       deps: deps()
     ]
   end
@@ -24,6 +25,16 @@ defmodule Collections.MixProject do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      name: :collections,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["stackcats"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/stackcats/collections"}
     ]
   end
 end
