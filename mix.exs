4188bfd58b1d6fe1af138c4e71721306f5eefc71
stackcats <stackcats@foxmail.com>
2021-11-11 23:59:47 +0800

    feat: add description to mix.exs

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Collections.MixProject do
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/stackcats/collections",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -26,6 +28,12 @@ defmodule Collections.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp description do
+    """
+    A library provides efficient implementations of the most common general purpose programming data structures.
+    """
   end
 
   defp package() do
